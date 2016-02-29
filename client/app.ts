cdd15878b6ee6a768018628dd69bb3b6aa06e79d
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2016-02-28 21:27:58 -0300

    Adding and removing objects

--- client/app.ts
+++ client/app.ts
@@ -4,17 +4,24 @@ import {bootstrap} from 'angular2-meteor';
 
 import {Parties} from 'collections/parties';
 
+import {PartiesForm} from 'client/parties-form/parties-form';
+
 @Component({
   selector: 'app'
 })
 @View({
-  templateUrl: 'client/app.html'
+  templateUrl: 'client/app.html',
+  directives: [PartiesForm]
 })
 class Socially {
   parties: Mongo.Cursor<Object>;
   
   constructor () {
     this.parties = Parties.find();
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
   }
 }
 
